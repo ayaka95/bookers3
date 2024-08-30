638572f71f8cdbc9f98ef31be642606473ac9ab9
ayaka95 <172455399+ayaka95@users.noreply.github.com>
2024-08-30 02:20:57 +0000

    user_id追加

---
+++ db/migrate/20240829120901_remove_user_id_from_books.rb
@@ -0,0 +1,5 @@
+class RemoveUserIdFromBooks < ActiveRecord::Migration[6.1]
+  def change
+
+  end
+end
---
+++ db/migrate/20240829130638_rename_user_id_to_books.rb
@@ -0,0 +1,4 @@
+class RenameUserIdToBooks < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
